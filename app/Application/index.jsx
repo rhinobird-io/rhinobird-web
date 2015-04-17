49a4b1bb44100eb6055e7d0bd7e5f913acf09881
Liu Yang <liu_yang@worksap.co.jp>
2015-04-17 15:19:38 +0800

    Add material UI

--- app/Application/index.jsx
+++ app/Application/index.jsx
@@ -2,8 +2,17 @@ var React = require("react");
 var StateFromStoreMixin = require("items-store/StateFromStoresMixin");
 var RouteHandler = require("react-router").RouteHandler;
 var MainMenu = require("./MainMenu.jsx");
+var injectTapEventPlugin = require("react-tap-event-plugin");
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
-require("./style.css");
+require("./style.less");
+
+var mui = require('material-ui');
+
 
 var Application = React.createClass({
 	mixins: [StateFromStoreMixin],
@@ -19,6 +28,7 @@ var Application = React.createClass({
 		return <div className={this.state.loading ? "application loading" : "application"}>
 			{this.state.loading ? <div style={{float: "right"}}>loading...</div> : null}
 			<h1>react-starter</h1>
+            <mui.RaisedButton label="Default"/>
 			<MainMenu />
 			<button onClick={this.update}>Update todolist data</button>
 			<RouteHandler />
