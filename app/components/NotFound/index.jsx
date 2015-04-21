05d00a374265f9ddf625e8e0e88071beff0a274a
Liu Yang <liu_yang@worksap.co.jp>
2015-04-21 16:29:17 +0800

    Improve not found page

--- app/components/NotFound/index.jsx
+++ app/components/NotFound/index.jsx
@@ -1,10 +1,15 @@
 var React = require("react");
 
+
+require('./style.less');
+
 module.exports = React.createClass({
 	render: function() {
-		return <div>
-			<h2>Not found</h2>
-			<p>The page you requested was not found.</p>
+		return <div className="notFound">
+            <div>
+                <h2>Not found</h2>
+                <p>The page you requested was not found.</p>
+            </div>
 		</div>;
 	}
 });
