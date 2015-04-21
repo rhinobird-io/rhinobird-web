e527416e87b5798bdbfbf8ebe9d3e2f0befec366
wizawu <wizawu@gmail.com>
2015-04-21 10:25:52 +0800

    Add smart-display

---
+++ app/components/SmartEditor/SmartEditor.jsx
@@ -0,0 +1,6 @@
+"use strict";
+
+var React = require("react");
+
+export default React.createClass({
+});
---
+++ app/components/SmartEditor/SmartPreview.jsx
@@ -0,0 +1,9 @@
+"use strict";
+
+var React = require("react");
+
+export default React.createClass({
+	render() {
+		let value = this.props.value;
+	}
+});
