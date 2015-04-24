324b71750e5fc2af4cb255d18b4295abe3b5ddce
wizawu <wizawu@gmail.com>
2015-04-24 16:02:09 +0800

    Notification layout

--- app/components/Notification/index.jsx
+++ app/components/Notification/index.jsx
@@ -5,10 +5,38 @@ const mui = require("material-ui"),
       IconButton = mui.IconButton;
 
 const DropDownAny = require("../DropDownAny");
+const Avatar = require("../Member").Avatar;
+const Name = require("../Member").Name;
 const NotificationActions = require("../../actions/NotificationActions");
 const NotificationStore = require("../../stores/NotificationStore");
+const SmartTimeDisplay = require("../SmartTimeDisplay");
 
 require("./style.less");
+
+let NotifiItem = React.createClass({
+  propTypes: {
+    sender: React.PropTypes.object.isRequired,
+    time: React.PropTypes.object.isRequired,
+    message: React.PropTypes.string.isRequired
+  },
+
+  render() {
+    return (
+      <div className="horizontal layout">
+        <div className="avatar-wrapper">
+          <Avatar scale={1.6} member={this.props.sender} />
+        </div>
+        <div className="vertical layout">
+          <div className="horizontal layout">
+            <div className="name"><Name member={this.props.sender} /></div>
+            <div className="time"><SmartTimeDisplay start={this.props.time} relative /></div>
+          </div>
+          <div className="message">{this.props.message}</div>
+        </div>
+      </div>
+    );
+  }
+});
 
 export default React.createClass({
 
@@ -34,7 +62,14 @@ export default React.createClass({
 
   render() {
     let control = <IconButton iconClassName="icon-notifications" />;
-    let menu = [1,2,3,4,5].map(i => <span>{i}</span>);
+    let menu = [1,2,3,4,5].map(i => {
+      let sender = {
+        name: "wizawu",
+        realname: "Wu Hualiang",
+        hash: "9dce167332fc0177c30bcc13a5f3ee89"
+      };
+      return <NotifiItem sender={sender} time={new Date()} message={"Hello long long long long long long long long long long"} />;
+    });
     return <DropDownAny control={control} menu={menu} menuClasses="notification-menu" />
   }
 });
