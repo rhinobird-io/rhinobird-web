374c4a88a6b12fdd83f2adbd39c52fb509b38aeb
wizawu <wizawu@gmail.com>
2015-05-04 15:55:20 +0800

    Fix popup filter. #26

--- app/components/SmartEditor/SmartEditor.jsx
+++ app/components/SmartEditor/SmartEditor.jsx
@@ -1,15 +1,14 @@
 "use strict";
 
 const React = require("react");
-const mui = require("material-ui"),
-      TextField = mui.TextField;
-
+const mui = require("material-ui"), TextField = mui.TextField;
 const CaretPosition = require("textarea-caret-position");
 
 const Item = require("../Flex").Item;
 const PopupSelect = require("../Select").PopupSelect;
+const UserStore = require("../../stores/UserStore");
 
-const commands = [
+const COMMANDS = [
   {name: "vity", manual: ":room_name"},
   {name: "file", manual: ":file_id"}
 ];
@@ -17,69 +16,103 @@ const commands = [
 export default React.createClass({
   propTypes: {
     disabled: React.PropTypes.bool,
-    users: React.PropTypes.array
+    popupWidth: React.PropTypes.number,
+    popupMaxHeight: React.PropTypes.number
+  },
+
+  getDefaultProps() {
+    return {
+      popupWidth: 240,
+      popupMaxHeight: 280
+    };
   },
 
   getInitialState() {
     return {
-      hidePopup: true,
+      options: this._getOptions(),
+      showPopup: false,
       popupPosition: {}
     };
   },
 
-  getValue() {
-    return this.refs.textfield.getValue();
+  componentDidMount() {
+    UserStore.addChangeListener(this._onChange);
+  },
+
+  componentWillUnmount() {
+    UserStore.removeChangeListener(this._onChange);
+  },
+
+  _onChange() {
+    this.setState({options: this._getOptions()});
   },
 
   _getOptions() {
-    return [1,2,4,5,6,9].map(x =>
-      <option value={x}><span>{x}</span></option>
-    );
+    return UserStore.getUsersArray().map(u =>
+      <option key={u.id} value={u.id} data={"@" + u.name}>
+        <span style={{fontWeight: 500}}>{u.name}</span>
+      </option>
+    ).concat(COMMANDS.map(c =>
+      <option key={c.name} value={c.name} data={"#" + c.name}>
+        <span style={{fontWeight: 500}}>{c.name}</span>
+        <span>{c.manual}</span>
+      </option>
+    ));
   },
 
-  _onChange(e) {
+  _onInputChange(e) {
+    this.refs.popup.filter(this.getValue());
     this._setPopupPosition(this.refs.textfield.refs.input.getInputNode());
   },
 
   _setPopupPosition(textarea) {
     let text = textarea.value;
     let selectionEnd = textarea.selectionEnd;
-    //console.log(text.charAt(selectionEnd - 1));
+    console.log(text.charAt(selectionEnd - 1));
+  },
+
+  getValue() {
+    return this.refs.textfield.getValue();
+  },
+
+  showPopup() {
+    this.setState({showPopup: true});
+  },
+
+  hidePopup() {
+    this.setState({showPopup: false});
   },
 
   render() {
-    let _this = this;
+    let style = {
+      padding: "0 0.5em 0.5em",
+      boxSizing: "border-box"
+    };
+    Object.assign(style, this.props.style);
+
     let popupStyle = {
-      width: 240,
-      maxHeight: 280,
+      width: this.props.popupWidth,
+      maxHeight: this.props.popupMaxHeight,
       overflow: "auto"
     };
     for (let k in this.state.popupPosition) {
       popupStyle[k] = this.state.popupPosition[k];
     }
 
-    let style = {
-      padding: "0 0.5em 0.5em",
-      boxSizing: "border-box"
-    };
-    Object.assign(style, this.props.style);
-    // `style` property for TextField seems useless
+    let popup = <PopupSelect ref="popup"
+            style={popupStyle}
+            controller={this.refs.textfield}
+            onKeyDown={() => 0}
+            onChange={this._onInputChange}>
+          {this.state.options}
+        </PopupSelect>
+
+    // Apply `style` to TextField seems no effect, so just apply to Item
     return (
       <Item flex style={style} className="smart-editor">
         <TextField {...this.props} ref="textfield" />
+        {popup}
       </Item>
-    );
-
-    return (
-      <div className="smart-editor">
-        <TextField {...this.props} ref="textfield" />
-        <PopupSelect styles={popupStyle}
-            controller={this.refs.textfield}
-            onKeyDown={() => 0}
-            onChange={this._onChange}>
-          {this._getOptions()}
-        </PopupSelect>
-      </div>
     );
   }
 });
