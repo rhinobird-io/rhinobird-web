d648a9cf112786b0eeb2c349fc33e06c2455178a
Liu Yang <liu_yang@worksap.co.jp>
2015-05-05 19:39:23 +0800

    Improve performance on dashboard.

--- app/components/Dashboard/DashboardRecord/index.jsx
+++ app/components/Dashboard/DashboardRecord/index.jsx
@@ -1,4 +1,4 @@
-const React = require("react");
+const React = require("react/addons");
 const Member = require('../../Member');
 const Flex = require('../../Flex');
 const SmartTimeDisplay = require('../../SmartTimeDisplay');
@@ -7,16 +7,17 @@ require('./style.less');
 
 const UserStore = require('../../../stores/UserStore');
 export default React.createClass({
+    mixins: [React.addons.PureRenderMixin],
 	render: function() {
         let record = this.props.record;
-        let member = UserStore.getUser(record.from_user_id);
+        let member = UserStore.getUser(record.get('from_user_id'));
 		return <Flex.Layout className='dashboard-record'>
 			<div className='avatar'><Member.Avatar scale={1.5} member={member}/></div>
             <div className='text'>
                 <Member.Name member={member}/><span className="time mui-font-style-caption"><SmartTimeDisplay relative start={record.created_at}></SmartTimeDisplay></span>
                 <div>
-                    <span>{record.content} </span>
-                    {record.hasLink? <a href={record.link_url}>{record.link_title}</a>: undefined}
+                    <span>{record.get('content')} </span>
+                    {record.get('hasLink')? <a href={record.get('link_url')}>{record.get('link_title')}</a>: undefined}
                 </div>
             </div>
 		</Flex.Layout>;
