6382d4af0e0a2d0761a2a491e841f0f3a4dfe611
wizawu <wizawu@gmail.com>
2015-05-06 16:40:18 +0800

    Fix websocket API. #34

--- app/actions/NotificationActions.js
+++ app/actions/NotificationActions.js
@@ -7,7 +7,7 @@ const NotificationStore = require("../stores/NotificationStore");
 
 function _websocket() {
   try {
-    let socket = new WebSocket("ws://" + window.location.host + "/platform/");
+    let socket = new WebSocket("ws://" + window.location.host + "/platform/api/");
     socket.onmessage = msg => {
       AppDispatcher.dispatch({
         type: ActionTypes.RECEIVE_NOTIFI,
