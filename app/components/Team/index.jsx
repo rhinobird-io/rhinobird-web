570ee1bf7ec36765518938bca6162710062aef57
Liu Yang <liu_yang@worksap.co.jp>
2015-05-06 11:54:03 +0800

    Fix key problem of Team page

--- app/components/Team/index.jsx
+++ app/components/Team/index.jsx
@@ -1,6 +1,6 @@
 'use strict';
 
-const React = require('react'),
+const React = require('react/addons'),
     mui = require('material-ui'),
     Paper = mui.Paper;
 
@@ -12,6 +12,7 @@ const Member = require('../Member');
 require("./style.less");
 
 let TeamDisplay = React.createClass({
+    mixins: [React.addons.PureRenderMixin],
     render: function () {
         if (this.props.team) {
             return <div className='paper-outer-container'>
@@ -22,8 +23,8 @@ let TeamDisplay = React.createClass({
                             <div style={{marginLeft: 8}}>{this.props.team.name}</div>
                         </Flex.Layout>
                         <Flex.Layout wrap>
-                            {this.props.team.users.map((user)=> {
-                                return <div style={{margin: 6}}>
+                            {this.props.team.users.map((user, index)=> {
+                                return <div style={{margin: 6}} key={index}>
                                     <Member.Avatar member={user}/>
                                     <Member.Name style={{marginLeft: 4}} member={user}/>
                                 </div>;
@@ -38,7 +39,8 @@ let TeamDisplay = React.createClass({
     }
 });
 
-module.exports = React.createClass({
+let TeamPage = React.createClass({
+    mixins: [React.addons.PureRenderMixin],
     componentDidMount(){
         this.props.setTitle('Team');
         UserStore.addChangeListener(this._userChanged);
@@ -54,9 +56,11 @@ module.exports = React.createClass({
         let loginUser = LoginStore.getUser();
         let teams = UserStore.getTeamsByUserId(loginUser.id);
         return <Flex.Layout perfectScroll fit centerJustified wrap className='teamPage'>
-            {teams.map((t)=> {
-                return <TeamDisplay team={t}/>
+            {teams.map((t, index)=> {
+                return <TeamDisplay team={t} key={index}/>
             })}
         </Flex.Layout>;
     }
 });
+
+export default TeamPage;
