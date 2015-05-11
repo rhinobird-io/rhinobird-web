91297a99627f585460a7972ab182b3adafb8eda5
Liu Yang <liu_yang@worksap.co.jp>
2015-05-11 12:37:49 +0800

    Fix drag

--- app/components/Team/index.jsx
+++ app/components/Team/index.jsx
@@ -217,8 +217,8 @@ let TeamGraph = React.createClass({
             } else {
                 return '';
             }
-        }).on('click', this.props.onClickTeam);
-        let circle = node.append('circle').attr('r', 14).attr('fill', 'white').attr('stroke', 'black').call(force.drag).on('click', this.props.onClickTeam);
+        }).call(force.drag).on('click', this.props.onClickTeam);
+        let circle = node.append('circle').attr('r', 14).attr('fill', 'white').attr('stroke', 'black').on('click', this.props.onClickTeam);
         let icon = node.append('text').attr('class', 'group-icon').attr('x', -10).attr('y', '.31em').text("\ue8d8").on('click', this.props.onClickTeam);
         let text = node.append('text').attr('x', 20).attr('y', '.31em').text(function (d) {
             return d.name;
