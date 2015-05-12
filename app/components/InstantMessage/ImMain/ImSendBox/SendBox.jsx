41f67a23458b6cec47baa118b8545ed13b7e3db5
Liu Yang <liu_yang@worksap.co.jp>
2015-05-12 16:02:15 +0800

    add emoji manual

--- app/components/InstantMessage/ImMain/ImSendBox/SendBox.jsx
+++ app/components/InstantMessage/ImMain/ImSendBox/SendBox.jsx
@@ -108,7 +108,7 @@ public static void main(String[] args) {
                     <p>1. ENTER to send</p>
                     <p>2. SHIFT + ENTER to have newline</p>
                     <p>3. Markdown support</p>
-                    <p>4. '@' to mention a member, '#' to use commands</p>
+                    <p>4. '@' to mention a member, '#' to use commands, ':' to use emoji</p>
                     <p>5. Markdown support</p>
                     <br/>
                     <div className='mui-font-style-title'>Example:</div>
