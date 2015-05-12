e721049f57e27cac5e1fcda94ebe4fc68352742a
Liu Yang <liu_yang@worksap.co.jp>
2015-05-12 11:04:55 +0800

    Improve style

--- app/components/InstantMessage/ImMain/ImSendBox/SendBox.jsx
+++ app/components/InstantMessage/ImMain/ImSendBox/SendBox.jsx
@@ -124,9 +124,9 @@ public static void main(String[] args) {
                     </Layout>
                 </Dialog>
 
-                <Layout>
+                <Layout style={{marginLeft: 44}} center>
                     <SmartEditor ref="sEditor" nohr multiLine valueLink={this.linkState('messageValue')} className="instant-message-smart-editor" onKeyDown={this.handleKeyDown}></SmartEditor>
-                    <IconButton className="icon-info-outline" style={{ fontSize:'2em' }} onClick={this.showInfoDialog}></IconButton>
+                    <IconButton className="icon-info-outline" style={{ fontSize:'2em',marginBottom: '6.5px' }} onClick={this.showInfoDialog}></IconButton>
                 </Layout>
             </div>
         );
