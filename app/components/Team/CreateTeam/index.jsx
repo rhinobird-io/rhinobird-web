2a80e3e0a391a99d5a30845fb7a4b808f70859c6
Liu Yang <liu_yang@worksap.co.jp>
2015-05-18 17:46:04 +0800

    Use member-select

--- app/components/Team/CreateTeam/index.jsx
+++ app/components/Team/CreateTeam/index.jsx
@@ -7,7 +7,8 @@ const React           = require("react"),
       Navigation      = Router.Navigation,
       Selector        = require("../../Select").Selector,
       PerfectScroll   = require('../../PerfectScroll'),
-      CalendarActions = require("../../../actions/CalendarActions");
+      CalendarActions = require("../../../actions/CalendarActions"),
+      MemberSelect = require('../../Member').MemberSelect;
 
 require("./style.less");
 
@@ -47,26 +48,26 @@ export default React.createClass({
                 <Flex.Layout horizontal centerJustified wrap>
                     <form onSubmit={this._handleSubmit}>
                     <MUI.Paper zDepth={3} className="create-team">
-                        <div style={{padding: 24}}>
+                        <div style={{padding: 24, width: 400}}>
                             <h3>Create Team</h3>
                             <MUI.TextField
                                 ref="teamName"
                                 hintText="Team name"
                                 floatingLabelText="Name"
+                                errorText={this.state.nameError}
                                 valueLink={this.linkState("name")}
                                 className="create-team-textfield" />
-                            <MUI.TextField
-                                hintText="Parent teams"
-                                floatingLabelText="Parent teams"
-                                className="create-team-textfield" />
-                            <MUI.TextField
-                                hintText="Subsidiary teams"
-                                floatingLabelText="Subsidiary teams"
-                                className="create-team-textfield" />
-                            <MUI.TextField
-                                hintText="Members"
-                                floatingLabelText="Members"
-                                className="create-team-textfield" />
+                            <MemberSelect
+                                floatingLabelText='Parent teams'
+                                valueLink={this.linkState('parentTeams')} user={false} className="create-team-textfield"/>
+                            <MemberSelect
+                                floatingLabelText='Subsidiary teams'
+                                valueLink={this.linkState('subTeams')}
+                                user={false} className="create-team-textfield"/>
+                            <MemberSelect
+                                floatingLabelText='Direct members'
+                                valueLink={this.linkState('members')}
+                                team={false} className="create-team-textfield"/>
                             <Flex.Layout horizontal justified>
                                 <Link to="team">
                                     <MUI.RaisedButton label="Cancel" />
@@ -84,5 +85,11 @@ export default React.createClass({
 
     _handleSubmit(e) {
         e.preventDefault();
+        console.log(this.state);
+        if(!this.state.name) {
+            this.setState({
+                nameError: 'Name should not be empty'
+            })
+        }
     }
 });
