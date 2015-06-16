1530c0b1f5c0c09f43a8db987b1e8e1b429066cf
RonaldoLee <ljxnegod@hotmail.com>
2015-06-16 18:06:32 +0800

    Resource routes.

--- app/mainRoutes.jsx
+++ app/mainRoutes.jsx
@@ -33,6 +33,10 @@ module.exports = (
           <Route name="talk" key="imTalk" path="talk/:backEndChannelId" handler={ require("react-router-proxy!./components/InstantMessage/ImMain/Main") }/>
           <Route name="setting" path="setting" handler={require("react-proxy!./components/InstantMessage/ImSetting")} />
         </Route>
+        <Route name="resource" path="resource" handler={require("react-proxy!./components/Resource")}>
+            <Route name="resources" path="resources" handler={require("react-proxy!./components/Resource/ResourceList")} />
+            <DefaultRoute handler={require("react-proxy!./components/Resource/ResourceList")} />
+        </Route>
         <Route name="post" path="post" handler={require("react-proxy!./components/Post")}>
         </Route>
         <Route name="post-detail" path="post/:id" handler={require("react-router-proxy!./components/Post/PostDetail")}>
