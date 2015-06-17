a041a7673b81dcbb3ad1614d185980aaa349a8aa
RonaldoLee <ljxnegod@hotmail.com>
2015-06-17 13:03:04 +0800

    Resource list.

--- app/components/Resource/ResourceList.jsx
+++ app/components/Resource/ResourceList.jsx
@@ -1,8 +1,17 @@
 const React = require("react");
 const ResourceStore = require('../../stores/ResourceStore');
 const ResourceAction = require('../../actions/ResourceActions');
+const Flex = require('../Flex');
+const PerfectScroll = require('../PerfectScroll');
+const InfiniteScroll = require('../InfiniteScroll');
+const MUI = require('material-ui');
+const Common = require('../Common');
 
-module.exports = React.createClass({
+let ResourceList = React.createClass({
+    contextTypes: {
+        muiTheme: React.PropTypes.object
+    },
+
     getInitialState() {
         return {
             resources: []
@@ -19,8 +28,34 @@ module.exports = React.createClass({
     },
 
     render() {
+        let resources = this.state.resources;
+        let content = null;
+
+        if (resources.length > 0){
+            content = (
+                <Flex.Layout wrap>
+                    {
+                        resources.map((resource, index) => (
+                            <MUI.Paper style={{flex: "1 1 320px", margin: 20, maxWidth: "50%"}}>
+                                <Flex.Layout vertical>
+                                    <div style={{height: 200, backgroundColor: this.context.muiTheme.palette.primary3Color}}>
+                                    </div>
+                                    <div style={{padding: "10px 12px"}}>
+                                        <Common.Display type="body2">{resource.name}</Common.Display>
+                                        <div style={{color: this.context.muiTheme.palette.disabledColor}}>Shanghai</div>
+                                    </div>
+                                </Flex.Layout>
+                            </MUI.Paper>
+                        ))
+                    }
+                </Flex.Layout>
+            );
+        } else {
+        }
         return (
-            <div>{this.state.resources.length}</div>
+            <PerfectScroll style={{padding: 24, position: 'relative'}}>
+                {content}
+            </PerfectScroll>
         );
     },
 
@@ -30,3 +65,5 @@ module.exports = React.createClass({
         });
     }
 });
+
+module.exports = ResourceList;
