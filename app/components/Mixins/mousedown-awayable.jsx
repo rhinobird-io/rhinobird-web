5739546e5adb30b618f605f0b665d84007512f82
RonaldoLee <ljxnegod@hotmail.com>
2015-06-21 17:17:45 +0800

    Add mousedownawayable mixins.

---
+++ app/components/Mixins/mousedown-awayable.jsx
@@ -0,0 +1,36 @@
+var React = require('react');
+var Events = require('material-ui/lib/utils/events');
+var Dom = require('material-ui/lib/utils/dom');
+
+module.exports = {
+
+    //When the component mounts, listen to MouseDown events and check if we need to
+    //Call the componentMouseDownAway function.
+    componentDidMount: function() {
+        if (!this.manuallyBindMouseDownAway) this._bindMouseDownAway();
+    },
+
+    componentWillUnmount: function() {
+        this._unbindMouseDownAway();
+    },
+
+    _checkMouseDownAway: function(e) {
+        var el = React.findDOMNode(this);
+
+        // Check if the target is inside the current component
+        if (e.target != el &&
+            !Dom.isDescendant(el, e.target) &&
+            document.documentElement.contains(e.target)) {
+            if (this.componentMouseDownAway) this.componentMouseDownAway();
+        }
+    },
+
+    _bindMouseDownAway: function() {
+        Events.on(document, 'mousedown', this._checkMouseDownAway);
+    },
+
+    _unbindMouseDownAway: function() {
+        Events.off(document, 'mousedown', this._checkMouseDownAway);
+    }
+
+};
