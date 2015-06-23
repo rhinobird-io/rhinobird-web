bbe06170a7b46228a59fd4f597e731c9d61ebb63
RonaldoLee <ljxnegod@hotmail.com>
2015-06-23 14:10:15 +0800

    Navigate before/next under week/day/fourdays view.

--- app/components/Calendar/CommonComponents/CalendarView.jsx
+++ app/components/Calendar/CommonComponents/CalendarView.jsx
@@ -1,4 +1,5 @@
 const React = require('react');
+const MUI = require('material-ui');
 const Flex = require('../../Flex');
 const WeekView = require('./WeekView');
 const DaysView = require('./DaysView');
@@ -6,6 +7,18 @@ const FourDaysView = require('./FourDaysView');
 const Selector = require('../../Select').Selector;
 
 let VIEW_TYPES = ['week', 'month', 'day', 'fourDays'];
+let BEFORE_TOOLTIPS = {
+    'week': 'Last Week',
+    'month': 'Last Month',
+    'day': 'Last Day',
+    'fourDays': 'Last Four Days',
+};
+let NEXT_TOOLTIPS = {
+    'week': 'Next Week',
+    'month': 'Next Month',
+    'day': 'Next Day',
+    'fourDays': 'Next Four Days',
+};
 
 let CalendarView = React.createClass({
     propType: {
@@ -54,6 +67,7 @@ let CalendarView = React.createClass({
         } else if (viewType === "month") {
 
         }
+
         return (
             <Flex.Layout vertical>
                 {calendarView}
@@ -64,13 +78,76 @@ let CalendarView = React.createClass({
 
     _getViewTypeSwitcher() {
         let viewTypes = VIEW_TYPES.map(type => <span name={type}>{type}</span>);
+        let tooltipNavigateBefore = BEFORE_TOOLTIPS[this.state.viewType];
+        let tooltipNavigateNext = NEXT_TOOLTIPS[this.state.viewType];
+        let isTodayInView = false;
+        if (this.state.date.toDateString() === new Date().toDateString()) {
+            isTodayInView = true;
+        }
         return (
-            <Flex.Layout center centerJustified style={{minHeight: 40, borderTop: "1px solid " + muiTheme.palette.borderColor}}>
-                <Selector value={this.state.viewType} onSelectChange={v => this.setState({viewType: v})}>
-                    {viewTypes}
-                </Selector>
+            <Flex.Layout horizontal center style={{minHeight: 48, borderTop: "1px solid " + muiTheme.palette.borderColor}}>
+                <Flex.Layout flex={1}>
+                    <MUI.FlatButton
+                        label="Go To Today"
+                        onClick={this._gotoToday}
+                        disabled={isTodayInView}
+                        secondary={!isTodayInView}/>
+                </Flex.Layout>
+                <Flex.Layout flex={1} center centerJustified>
+                    <MUI.IconButton
+                        title={tooltipNavigateBefore}
+                        onClick={this._navigateBefore}
+                        iconClassName="icon-navigate-before" />
+                    <Selector value={this.state.viewType} onSelectChange={v => this.setState({viewType: v})}>
+                        {viewTypes}
+                    </Selector>
+                    <MUI.IconButton
+                        title={tooltipNavigateNext}
+                        onClick={this._navigateNext}
+                        iconClassName="icon-navigate-next" />
+                </Flex.Layout>
+                <Flex.Layout flex={1}>
+                </Flex.Layout>
             </Flex.Layout>
         );
+    },
+
+    _gotoToday() {
+        this.setState({
+            date: new Date()
+        })
+    },
+
+    _navigateBefore() {
+        let viewType = this.state.viewType;
+        let date = this.state.date;
+
+        if (viewType === "week") {
+            date.setDate(date.getDate() - 7);
+        } else if (viewType === "day") {
+            date.setDate(date.getDate() - 1);
+        } else if (viewType === "fourDays") {
+            date.setDate(date.getDate() - 4);
+        }
+        this.setState({
+            date: date
+        });
+    },
+
+    _navigateNext() {
+        let viewType = this.state.viewType;
+        let date = this.state.date;
+
+        if (viewType === "week") {
+            date.setDate(date.getDate() + 7);
+        } else if (viewType === "day") {
+            date.setDate(date.getDate() + 1);
+        } else if (viewType === "fourDays") {
+            date.setDate(date.getDate() + 4);
+        }
+        this.setState({
+            date: date
+        });
     }
 });
 
