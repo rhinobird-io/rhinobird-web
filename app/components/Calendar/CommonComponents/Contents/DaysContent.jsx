a57d88b53db81e6be455350d0e95e233886b92c2
RonaldoLee <ljxnegod@hotmail.com>
2015-06-23 15:20:22 +0800

    Cancel resoure booking button and function.

--- app/components/Calendar/CommonComponents/Contents/DaysContent.jsx
+++ app/components/Calendar/CommonComponents/Contents/DaysContent.jsx
@@ -13,6 +13,15 @@ let DaysContent = React.createClass({
         muiTheme: React.PropTypes.object
     },
 
+    cancelCreateNewRange() {
+        for (let i = 0; i < this.props.dates.length; i++) {
+            let day = this.refs["day" + i];
+            if (day) {
+                day.cancelCreateNewRange();
+            }
+        }
+    },
+
     getDefaultProps() {
         return {
             data: []
@@ -22,8 +31,8 @@ let DaysContent = React.createClass({
     render() {
         let {
             data,
-            onRectCreate,
-            onRectCancel,
+            onRangeCreate,
+            onRangeCancel,
             awayExceptions,
             ...others
         } = this.props;
@@ -34,11 +43,13 @@ let DaysContent = React.createClass({
                 borderLeft: "1px solid " + this.context.muiTheme.palette.borderColor
             }
         };
-        let daysContent = this.props.dates.map(d => (
+
+        let daysContent = this.props.dates.map((d, index) => (
             <Flex.Layout flex={1} style={styles.dayContent} >
                 <DayContent
-                    onRectCreate={onRectCreate}
-                    onRectCancel={onRectCancel}
+                    ref={"day" + index}
+                    onRangeCreate={onRangeCreate}
+                    onRangeCancel={onRangeCancel}
                     awayExceptions={awayExceptions}
                     date={Moment(d).format("YYYY-MM-DD")}
                     data={data.filter(_ => new Date(_.fromTime).toDateString() == d.toDateString())} />
