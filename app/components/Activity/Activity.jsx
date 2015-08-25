454b891e26e6775ea18e595abd6242006b50da19
Liu Yang <liu_yang@worksap.co.jp>
2015-08-25 11:15:32 +0800

    Add scroll, title.

--- app/components/Activity/Activity.jsx
+++ app/components/Activity/Activity.jsx
@@ -1,11 +1,20 @@
 const React = require("react");
 const RouteHandler = require("react-router").RouteHandler;
+const PerfectScroll = require("../PerfectScroll");
 
 module.exports = React.createClass({
 
+    componentDidMount() {
+        this.props.setTitle("Activity");
+    },
     render(){
         return (
-            <div>Test</div>
+            <PerfectScroll style={{height: '100%', position:'relative', padding:24}}>
+                <div>Test</div>
+                <div>Test</div>
+                <div>Test</div>
+                <div>Test</div>
+            </PerfectScroll>
         );
     }
 });
