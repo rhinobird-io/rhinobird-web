1090f822e1a0160223973ff0a8de197e0fe1a267
Liu Yang <liu_yang@worksap.co.jp>
2015-08-26 11:14:07 +0800

    Add next coming and my activities button

--- app/components/Activity/Activity.jsx
+++ app/components/Activity/Activity.jsx
@@ -16,17 +16,26 @@ module.exports = React.createClass({
         this.props.setTitle("Activity");
     },
     render(){
+        let u = UserStore.getUser(1);
         return (
             <PerfectScroll style={{height: '100%', position:'relative', padding:24}}>
                 <div style={{margin:'0 auto', maxWidth:1000}}>
                 <Flex.Layout justified>
                     <Flex.Item flex={1}>
                         <mui.Paper style={{padding:24, width:'100%'}}>
-                            <Common.Display type='display3' style={{color:this.context.muiTheme.palette.primary1Color}}>325</Common.Display>
-                            <Common.Display type='display3' style={{marginLeft:24}}>Points</Common.Display>
+                            <Flex.Layout start justified>
+                                <div>
+                                    <Common.Display type='display3' style={{color:this.context.muiTheme.palette.primary1Color}}>325</Common.Display>
+                                    <Common.Display type='display3' style={{marginLeft:24}}>Points</Common.Display>
+                                </div>
+                                <mui.RaisedButton primary={true} label="My activities"/>
+                            </Flex.Layout>
+                            <Common.Hr style={{margin: '24px 0'}}/>
+                            <Common.Display type='body3'>Next coming</Common.Display>
+                            <ListItem style={{left:-16}} secondaryText="1000" key={u.id} leftAvatar={<Member.Avatar scale={1.6666667} link={false} member={u}/>}><Member.Name link={false} member={u}/></ListItem>
                         </mui.Paper>
 
-                        <mui.Paper style={{padding:6, width:'100%', marginTop:48}}>
+                        <mui.Paper style={{padding:8, width:'100%', marginTop:48}}>
                             <mui.List subheader="Speech">
                                 {UserStore.getUsersArray().slice(0,10).map(u=>{
                                     return <ListItem secondaryText="1000" key={u.id} leftAvatar={<Member.Avatar scale={1.6666667} link={false} member={u}/>}><Member.Name link={false} member={u}/></ListItem>
@@ -35,7 +44,7 @@ module.exports = React.createClass({
                         </mui.Paper>
                     </Flex.Item>
                     <div style={{marginLeft:48}}>
-                        <mui.Paper style={{padding:6}}>
+                        <mui.Paper style={{padding:8}}>
                             <mui.List subheader="Rank">
                                 {UserStore.getUsersArray().slice(0,10).map(u=>{
                                     return <ListItem secondaryText="1000" key={u.id} leftAvatar={<Member.Avatar scale={1.6666667} link={false} member={u}/>}><Member.Name link={false} member={u}/></ListItem>
