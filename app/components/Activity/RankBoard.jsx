f75499a1a558757a91e204f9f0f6e2905eeca73c
Liu Yang <liu_yang@worksap.co.jp>
2015-09-01 16:47:51 +0800

    Fix rank board to point_total.

--- app/components/Activity/RankBoard.jsx
+++ app/components/Activity/RankBoard.jsx
@@ -18,8 +18,8 @@ module.exports = React.createClass({
             this.setState({
                 rankedUsers: data.map((u)=>{
                     let user = UserStore.getUser(u.id);
-                    user.point = u.point;
-                    return user;
+                    Object.assign(u, user);
+                    return u;
                 })
             });
         });
@@ -32,7 +32,7 @@ module.exports = React.createClass({
             <mui.Paper style={{padding:8}}>
                 <mui.List subheader="Rank">
                     {this.state.rankedUsers.map(u=> {
-                        return <Member.Link member={u}><ListItem secondaryText={u.point} key={u.id}
+                        return <Member.Link member={u}><ListItem secondaryText={u.point_total} key={u.id}
                                          leftAvatar={<Member.Avatar scale={1.6666667} link={false} member={u}/>}><Member.Name
                             link={false} member={u}/></ListItem></Member.Link>
                     })}
