7f8645eb5a9355fe0418bae0081611548b4ccdb3
Liu Yang <liu_yang@worksap.co.jp>
2015-09-10 10:32:46 +0800

    Change comment to the bottom of page layout.

--- app/components/Activity/SpeechDetail/index.jsx
+++ app/components/Activity/SpeechDetail/index.jsx
@@ -294,15 +294,16 @@ module.exports = React.createClass({
                 {primaryBtn}
                 </Flex.Layout>;
 
-            speechComment = (<Flex.Layout vertical key="comments" style={styles.detailItem}>
-                <Flex.Layout center style={{margin: '20px 0px'}}><Common.Display type='title'>Comments</Common.Display></Flex.Layout>
-                <Flex.Layout vertical startJustified flex={1}>
-                    <Thread style={{width: "100%"}} threadKey={this.state.threadKey} threadTitle={`Comment ${speech.title}`} />
-                </Flex.Layout>
+            speechComment = (<Flex.Layout vertical key="comments" style={{
+                borderTop: `1px solid ${this.context.muiTheme.palette.borderColor}`,
+                padding: 24
+            }}>
+                <Common.Display type='title'>Comments</Common.Display>
+                <Thread style={{width: "100%"}} threadKey={this.state.threadKey} threadTitle={`Comment ${speech.title}`} />
             </Flex.Layout>);
             if (speech.status !== ActivityConstants.SPEECH_STATUS.CLOSED) {
                 stepBar = <Flex.Layout center vertical style={{borderLeft: '1px solid ' + this.context.muiTheme.palette.borderColor, width: 180, flexShrink:0}}>
-                    <StepBar vertical style={{padding:24, width:100, height:400}} activeStep={speechStatus.get(speech.status)} stepTitles={["New", "Auditing", "Approved", "Confirmed", "Finished"]}/>
+                    <StepBar vertical style={{padding:24, width:100, height:300}} activeStep={speechStatus.get(speech.status)} stepTitles={["New", "Auditing", "Approved", "Confirmed", "Finished"]}/>
                     {speechActions}
                 </Flex.Layout>
             }
@@ -329,12 +330,12 @@ module.exports = React.createClass({
                                 {speechAudiences}
                                 {speechContent}
 
-                                {speechComment}
                             </Flex.Item>
 
                             {stepBar}
                         </Flex.Layout>
 
+                        {speechComment}
                     </MUI.Paper>
                 </form>
                 <MUI.Paper zDepth={1} style={styles.selectTime}>
