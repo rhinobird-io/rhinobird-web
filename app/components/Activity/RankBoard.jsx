8d7567aa02a5dab1c3f5aeadeeb328cdea379647
Liu Yang <liu_yang@worksap.co.jp>
2015-09-14 11:08:43 +0800

    Fix rank point 0 problem

--- app/components/Activity/RankBoard.jsx
+++ app/components/Activity/RankBoard.jsx
@@ -32,7 +32,7 @@ module.exports = React.createClass({
             <mui.Paper style={{padding:8}}>
                 <mui.List subheader="Rank">
                     {this.state.rankedUsers.map(u=> {
-                        return <Member.Link member={u}><ListItem secondaryText={u.point_total} key={u.id}
+                        return <Member.Link member={u}><ListItem secondaryText={u.point_total.toString()} key={u.id}
                                          leftAvatar={<Member.Avatar scale={1.6666667} link={false} member={u}/>}><Member.Name
                             link={false} member={u}/></ListItem></Member.Link>
                     })}
@@ -41,4 +41,3 @@ module.exports = React.createClass({
         </div>;
     }
 });
-
