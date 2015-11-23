e800c1fc6f6758b20918312060d3c21bc1079fec
wangboxue <281637992@qq.com>
2015-11-23 10:51:35 +0800

    remove text-center css

--- app/helper/ActivityEmailHelper.js
+++ app/helper/ActivityEmailHelper.js
@@ -1,10 +1,10 @@
 export default {
     construct_email(name, content) {
         return `<div style="max-width: 600px; margin: auto;">
-            <h2 style="text-align: center; margin: 0 0 16px 0;">
+            <h2 style="margin: 0 0 16px 0;">
                 Hi, ${name}
             </h2>
-            <div style="font-size: 1.2em; text-align: center; line-height: 1.5em;">${content}</div>
+            <div style="font-size: 1.2em; line-height: 1.5em;">${content}</div>
             <br>
             <hr>
             <p style="text-align: center;">Designed by ATE-Shanghai, © Works Applications Co.,Ltd.</p>
