c8674c51fcbca83a24b2f7825f6cdfe5cc27b188
An Xiaoqiang <an_x@worksap.co.jp>
2017-06-22 20:12:14 +0800

    Support Quick Login from Genius Center

--- app/components/Signin/index.jsx
+++ app/components/Signin/index.jsx
@@ -26,10 +26,16 @@ var Login = React.createClass({
         }
     },
     componentWillMount(){
-        this.props.setTitle("RhinoBird");
-    },
-    componentDidMount() {
-        this.refs.email.focus();
+        var query = this.context.router.getCurrentQuery();
+        console.log(query);
+        if (query.ticket) {
+            $.post('/platform/api/genius_coming', {ticket: query.ticket, sign: query.sign}).then((data) => {
+                LoginAction.updateLogin(data);
+                this.context.router.transitionTo(this.context.router.getCurrentQuery().target || "/");
+            });
+        } else {
+            this.props.setTitle("RhinoBird");
+        }
     },
     _login(e){
         e.preventDefault();
@@ -46,17 +52,24 @@ var Login = React.createClass({
             });
         });
     },
+
+    _quick_login(){
+        window.location = 'http://genius.internal.worksap.com/login?app_id=rhinobird';
+    },
     render() {
         return <mui.Paper zDepth={1} className="loginForm" rounded={false}>
+            <Common.Link style={{margin: 24}}
+                         href='http://genius.internal.worksap.com/login?app_id=rhinobird'><h3 align="center">Quick Login
+                @GeniusCenter</h3></Common.Link>
+            <p>By using Quick Login, you can use one account to get service from many applications within only one time
+                authentication.</p>
             <form className="container" onSubmit={this._login}>
-                <h2 style={{marginBottom:24}}>Sign in</h2>
                 <mui.TextField ref="email" hintText='Email' valueLink={this.linkState('email')} autofocus/>
                 <mui.TextField hintText='Password' type="password" valueLink={this.linkState('password')}
-                    errorText={this.state.error? 'Email or password incorrect.' : undefined}/>
-                <div className="rightButton" style={{display:'flex', justifyContent:'space-between', alignItems:'center'}}>
-                    <Common.Link href='/platform/signup'>Sign up now!</Common.Link>
-                    <Common.Link href='http://mcenter.internal.worksap.com:3000/login?app_id=rhinobird'>Login from Member Center</Common.Link>
-                    <mui.RaisedButton label="Sign in" primary={true} onClick={this._login} type="submit"/>
+                               errorText={this.state.error ? 'Email or password incorrect.' : undefined}/>
+                <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                    <mui.RaisedButton label="Sign in" onClick={this._login} type="submit"/>
+                    <mui.RaisedButton label="Quick Login" onClick={this._quick_login} primary={true} type="submit"/>
                 </div>
             </form>
         </mui.Paper>;
