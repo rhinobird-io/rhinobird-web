c19e5770f6f046cd8df20345ccd4c655655a3015
SoniaXu <SoniaXu@users.noreply.github.com>
2019-08-13 14:57:01 +0800

    [xu_hai] add start date restriction on rankboard (#226)

--- app/components/Activity/RankBoard.jsx
+++ app/components/Activity/RankBoard.jsx
@@ -5,6 +5,7 @@ const Common = require('../Common');
 const UserStore = require('../../stores/UserStore');
 const ActivityIcon = require('./ActivityIcon');
 const Member = require('../Member');
+const moment = require('moment');
 
 module.exports = React.createClass({
 
@@ -14,9 +15,10 @@ module.exports = React.createClass({
         }
     },
     componentDidMount(){
-        $.post('/activity/users/rank').then((data)=> {
+        $.get(`/activity/attendances/points?start_at=${this.getStartDate()}`).then((data)=> {
             this.setState({
-                rankedUsers: data.map((u)=>{
+                rankedUsers: data.map((a)=>{
+                    let u = Object.assign(a, {id: a.user_id});
                     let user = UserStore.getUser(u.id);
                     Object.assign(u, user);
                     return u;
@@ -27,10 +29,13 @@ module.exports = React.createClass({
     contextTypes: {
         muiTheme: React.PropTypes.object
     },
+    getStartDate(){
+        return moment().subtract(90, 'days').format('YYYY-MM-DD');
+    },
     render(){
         return <div style={{marginLeft:48}}>
             <mui.Paper style={{padding:8}}>
-                <mui.List subheader="Rank">
+                <mui.List subheader={`Rank (from ${this.getStartDate()})`}>
                     {this.state.rankedUsers.map(u=> {
                         return <Member.Link member={u} key={u.id}><ListItem secondaryText={u.point_total.toString()}
                                          leftAvatar={<Member.Avatar scale={1.6666667} link={false} member={u}/>}><Member.Name
